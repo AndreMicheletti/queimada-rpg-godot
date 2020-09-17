5e2c34f940199f013a4aca7c1f030845c8bdb6f7
AndreMicheletti <andre.micheletti@zenvia.com>
2020-09-17 16:12:55 -0300

    added "Hot" state to ball with particle effects

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,51 +1,65 @@
 extends KinematicBody2D
 
-const STOP_THRESHOLD = 20.0
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var holded = false
+const STOP_THRESHOLD = 85.0
+const HOT_THRESHOLD = 320.0
+const DRAG = .97
+const HIT_DRAG = .80
 
+var holded = false
 var velocity = Vector2()
-var current_speed = 0
-var drag = 25
-var hit_drag = 100
 
 onready var collBox = $CollisionShape2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("ball")
-	
+
+
 func _physics_process(delta):
 	#if current_speed <= 5.0:
 	#	current_speed = 0
 	#	velocity = Vector2()
 	#	return
-	if abs(velocity.x) <= STOP_THRESHOLD and abs(velocity.y) <= STOP_THRESHOLD:
+	if not is_hot():
+		$FlyParticles.emitting = false
+	if velocity.length() <= STOP_THRESHOLD:
 		velocity = Vector2()
-	
+		$FlyParticles.emitting = false
+		return
+
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.normal)
-		current_speed -= hit_drag
-		if collision.collider.has_method("hit"):
+		reduce_velocity(HIT_DRAG)
+		if is_hot() and collision.collider.has_method("hit"):
 			collision.collider.hit()
-	velocity.x *= .94
-	velocity.y *= .94
+	reduce_velocity(DRAG)
+
+
+func reduce_velocity(ratio):
+	# ration must between 0 and 1 (example: if 0.97 will reduce velocity by 3%)
+	velocity.x *= ratio
+	velocity.y *= ratio
+
+
+func is_hot():
+	return velocity.length() > HOT_THRESHOLD
+
 
 func pick_up():
-	current_speed = 0
+	velocity = Vector2()
 	holded = true
 	collBox.disabled = true
-	
+
+
 func throw(pos, dir, speed):
 	holded = false
 	collBox.disabled = false
 	rotation = dir
 	# position = pos
-	current_speed = speed
 	velocity = Vector2(speed, 0).rotated(rotation)
+	$FlyParticles.emitting = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
