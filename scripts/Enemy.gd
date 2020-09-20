376bf65cef02da0bbbf3b77654a08f1cbab46016
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2020-09-20 20:43:05 -0300

    added tileset

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends "res://scripts/PhysicsObject.gd"
 
 const MOVE_SPEED = 200
 
@@ -7,21 +7,13 @@ onready var raycast = $RayCast2D
 var player = null
 
 func _ready():
-	add_to_group("zombies")
+	add_to_group("enemies")
 
 func _physics_process(delta):
-	if player == null:
-		return
-	
-	var vec_to_player = player.global_position - global_position
-	vec_to_player = vec_to_player.normalized()
-	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
-	move_and_collide(vec_to_player * MOVE_SPEED * delta)
-	
-	if raycast.is_colliding():
-		var coll = raycast.get_collider()
-		if coll.name == "Player":
-			coll.kill()
+	._physics_process(delta)
+
+func hit(velocity):
+	kill()
 
 func kill():
 	queue_free()
